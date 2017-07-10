912ad2aeb65b35c3ae8083f389011d9b2fb4e48c
Duri <2011305026@skuniv.ac.kr>
2017-07-10 15:50:21 +0900

    Key 입력으로 비행기 조종

--- Assets/Helicopter/Scripts/HelicopterController.cs
+++ Assets/Helicopter/Scripts/HelicopterController.cs
@@ -9,6 +9,7 @@ public class HelicopterController : MonoBehaviour
 
     public float TurnForce = 3f;
     public float ForwardForce = 10f;
+    public float SwingForce = 10f;
     public float ForwardTiltForce = 20f;
     public float TurnTiltForce = 30f;
     public float EffectiveHeight = 100f;
@@ -24,8 +25,8 @@ public class HelicopterController : MonoBehaviour
         set
         {
             HelicopterSound.pitch = Mathf.Clamp(value / 40, 0, 1.2f);
-            if (UIGameController.runtime.EngineForceView != null)
-                UIGameController.runtime.EngineForceView.text = string.Format("Engine value [ {0} ] ", (int)value);
+    //        if (UIGameController.runtime.EngineForceView != null)
+   //             UIGameController.runtime.EngineForceView.text = string.Format("Engine value [ {0} ] ", (int)value);
 
             _engineForce = value;
         }
@@ -50,31 +51,51 @@ public class HelicopterController : MonoBehaviour
     {
         LiftProcess();
         MoveProcess();
-        //TiltProcess();
+        //SwingProcess();
+        TiltProcess();
     }
 
+
+    //앞으로 가면서 턴
+    public float turn;
     private void MoveProcess()
     {
-        var turn = TurnForce * Mathf.Lerp(hMove.x, hMove.x * (turnTiltForcePercent - Mathf.Abs(hMove.y)), Mathf.Max(0f, hMove.y));
+        turn = TurnForce * Mathf.Lerp(hMove.x, hMove.x * (turnTiltForcePercent - Mathf.Abs(hMove.y)), Mathf.Max(0f, hMove.y));
         hTurn = Mathf.Lerp(hTurn, turn, Time.fixedDeltaTime * TurnForce);
         HelicopterModel.AddRelativeTorque(0f, hTurn * HelicopterModel.mass, 0f);
-        HelicopterModel.AddRelativeForce(Vector3.forward * Mathf.Max(0f, hMove.y * ForwardForce * HelicopterModel.mass));
+        //이동
+        HelicopterModel.AddRelativeForce(Vector3.forward * Mathf.Max(-1f, hMove.y * ForwardForce * HelicopterModel.mass));
     }
 
+    //위아래
+    public float upForce;
     private void LiftProcess()
     {
-        var upForce = 1 - Mathf.Clamp(HelicopterModel.transform.position.y / EffectiveHeight, 0, 1);
-        upForce = Mathf.Lerp(0f, EngineForce, upForce) * HelicopterModel.mass;
+        //Clamp -> 범위 지정(?) 0~1
+        upForce = 1 - Mathf.Clamp(HelicopterModel.transform.position.y / EffectiveHeight, -1f, 1f);
+        upForce = Mathf.Lerp(-1, EngineForce, upForce) * HelicopterModel.mass;
         HelicopterModel.AddRelativeForce(Vector3.up * upForce);
     }
 
+    //회전하는 연출
     private void TiltProcess()
-    {
+    {   //Lerp --> 점진적인 변화
+        //z축 기준으로 좌우로 기울기(여기선 x로 씌여져 있는데, 씬 환경이 달라서 그런듯)
         hTilt.x = Mathf.Lerp(hTilt.x, hMove.x * TurnTiltForce, Time.deltaTime);
+        //x축 기준으로 앞뒤로 기울기
         hTilt.y = Mathf.Lerp(hTilt.y, hMove.y * ForwardTiltForce, Time.deltaTime);
         HelicopterModel.transform.localRotation = Quaternion.Euler(hTilt.y, HelicopterModel.transform.localEulerAngles.y, -hTilt.x);
     }
 
+    //턴과 관련없이 좌우 이동
+    private void SwingProcess()
+    {
+        var turn = TurnForce * Mathf.Lerp(hMove.x, hMove.x * (turnTiltForcePercent - Mathf.Abs(hMove.x)), Mathf.Max(0f, hMove.x));
+        hTurn = Mathf.Lerp(hTurn, turn, Time.fixedDeltaTime * TurnForce);
+        HelicopterModel.AddRelativeTorque(0f, hTurn * HelicopterModel.mass, 0f);
+        HelicopterModel.AddRelativeForce(Vector3.right * hMove.x * SwingForce * HelicopterModel.mass);
+
+    }
     private void OnKeyPressed(PressedKeyCode[] obj)
     {
         if (!IsKeyInput) return;
